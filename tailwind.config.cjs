3b5cdf9aa1a8e01b0f6b30972f0e9a7988041d0c
floanita <anita.anita.263@gmail.com>
2026-06-24 18:24:06 +0200

    new web style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: ["light"],
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }
